893f931f9e162319862ad6405d0ed6abeedf302d
Ming Wai Chan <cmwdes@gmail.com>
2019-10-07 16:46:15 +0200

    async readback texture VS simple compute texture

--- Assets/ComputeUAVTexture/ComputeUAVTexture.cs
+++ Assets/ComputeUAVTexture/ComputeUAVTexture.cs
@@ -7,26 +7,26 @@ public class ComputeUAVTexture : MonoBehaviour
 
 	public ComputeShader shader;
 
+	private int size = 128;
 	private int _kernel;
-	private Material _mat;
+	public Material _mat;
 
 	void Start () 
 	{
 		_kernel = shader.FindKernel ("CSMain");
-		_mat = GetComponent<Renderer> ().material;
-		RunShader ();
-	}
-		
-	public void RunShader()
-	{
-		RenderTexture tex = new RenderTexture (512, 512, 24);
+
+		RenderTexture tex = new RenderTexture (size, size, 0);
 		tex.enableRandomWrite = true;
 		tex.Create ();
-
-		shader.SetTexture (_kernel, "Result", tex);
-		shader.Dispatch (_kernel, 512 / 8, 512 / 8, 1);
-
+		
 		_mat.SetTexture ("_MainTex", tex);
+		_mat = GetComponent<Renderer> ().material;
+		
+		shader.SetTexture (_kernel, "Result", tex);
+	}
 
+	void Update()
+	{
+		shader.Dispatch (_kernel, Mathf.CeilToInt(size / 8f), Mathf.CeilToInt(size / 8f), 1);
 	}
 }
