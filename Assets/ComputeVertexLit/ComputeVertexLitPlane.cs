6f70f6562ba50c5cea036786a7154d2bf76510a9
cinight <cinight@users.noreply.github.com>
2019-10-13 17:24:38 +0200

    better normal calculation

--- Assets/ComputeVertexLit/ComputeVertexLitPlane.cs
+++ Assets/ComputeVertexLit/ComputeVertexLitPlane.cs
@@ -21,11 +21,21 @@ public class ComputeVertexLitPlane : MonoBehaviour
     public Collider mc;
     private Mesh mesh;
 
+    //For the 2 maps
+    public int texResolution = 512;
+
     //Heightmap
-    private int sizeHeightMap = 256;
+    private RenderTexture tex;
     private int _kernelHeightMap;
     private Vector2Int dispatchCountHeightMap;
     public Material heightMapDebug;
+
+    //Normalmap
+    private RenderTexture texNor;
+    private int _kernelNormalMap;
+    private Vector2Int dispatchCountNormalMap;
+    public Material normalMapDebug;
+    public bool useMaterialNormal = true;
 
     //Compute
     public ComputeShader shader;
@@ -37,8 +47,8 @@ public class ComputeVertexLitPlane : MonoBehaviour
     //Mouse input
     private Camera cam;
     private RaycastHit hit;
-    private Vector3 mousePos;
-    private Vector3 defaultposition = new Vector3(0, 0, -99);
+    private Vector2 mousePos;
+    private Vector2 defaultposition = new Vector2(-9, -9); //make it far away
 
     void Start()
     {
@@ -67,45 +77,72 @@ public class ComputeVertexLitPlane : MonoBehaviour
         //Compute Shader kernel
         _kernel = shader.FindKernel ("CSMain");
         _kernelHeightMap = shader.FindKernel ("CSMainHeightMap");
+        _kernelNormalMap = shader.FindKernel ("CSMainNormalMap");
 
         //Dispatch counts
         uint threadX = 0;
         uint threadY = 0;
         uint threadZ = 0;
+        //kernel
         shader.GetKernelThreadGroupSizes(_kernel, out threadX, out threadY, out threadZ);
         dispatchCount = Mathf.CeilToInt(meshVertData.Length / threadX)+1;
+        //heightmap kernel
         shader.GetKernelThreadGroupSizes(_kernelHeightMap, out threadX, out threadY, out threadZ);
         dispatchCountHeightMap = Vector2Int.one;
-        dispatchCountHeightMap.x = Mathf.CeilToInt(sizeHeightMap / threadX)+1;
-        dispatchCountHeightMap.y = Mathf.CeilToInt(sizeHeightMap / threadY)+1;
+        dispatchCountHeightMap.x = Mathf.CeilToInt(texResolution / threadX)+1;
+        dispatchCountHeightMap.y = Mathf.CeilToInt(texResolution / threadY)+1;
+        //normalmap kernel
+        shader.GetKernelThreadGroupSizes(_kernelNormalMap, out threadX, out threadY, out threadZ);
+        dispatchCountNormalMap = Vector2Int.one;
+        dispatchCountNormalMap.x = Mathf.CeilToInt(texResolution / threadX)+1;
+        dispatchCountNormalMap.y = Mathf.CeilToInt(texResolution / threadY)+1;
 
         //heightmap texture
- 		RenderTexture tex = new RenderTexture (sizeHeightMap, sizeHeightMap, 0, GraphicsFormat.R8_UNorm);
+ 		tex = new RenderTexture (texResolution, texResolution, 0, GraphicsFormat.R8_UNorm);
 		tex.enableRandomWrite = true;
+        tex.wrapMode = TextureWrapMode.Clamp;
 		tex.Create ();
 		heightMapDebug.mainTexture = tex;
 
+        //normalmap texture
+ 		texNor = new RenderTexture (texResolution, texResolution, 0, GraphicsFormat.R8G8B8A8_UNorm);
+		texNor.enableRandomWrite = true;
+        texNor.wrapMode = TextureWrapMode.Clamp;
+		texNor.Create ();
+		normalMapDebug.mainTexture = texNor;
+
         //SetBuffer
-        shader.SetInt("_heightMapSize",sizeHeightMap);
-        shader.SetTexture(_kernelHeightMap, "heightMap", tex);
-        shader.SetTexture(_kernel, "heightMapTex", tex);
-        shader.SetBuffer(_kernel, "vertexBuffer", vertexBuffer);
+        shader.SetInt("_texResolution",texResolution);
+        shader.SetTexture(_kernelHeightMap, "heightMap", tex); // read & write
+        shader.SetTexture(_kernelNormalMap, "heightMapTex", tex); // readonly
+        shader.SetTexture(_kernelNormalMap, "normalMap", texNor); // read & write
+        shader.SetTexture(_kernel, "heightMapTex", tex); // readonly
+        shader.SetTexture(_kernel, "normalMapTex", texNor); // readonly
+        shader.SetBuffer(_kernel, "vertexBuffer", vertexBuffer); // read & write
 
         //The Material
         mat.name = "My Mat";
         mat.SetBuffer("vertexBuffer", vertexBuffer);
+
+        if(useMaterialNormal)
+        {
+            //Because normal map is only generated in runtime, 
+            //i.e. before playmode the material has no normal map, so Unity turns off this keyword.
+            mat.SetTexture("_BumpMap", texNor);
+            mat.EnableKeyword("_NORMALMAP");
+        }
     }
 
     void Update()
     {
-        //Getting mouse position
+        //Getting mouse position. MeshCollider is needed for getting hit.textureCoord
         if (
             Input.GetMouseButton(0) &&
             Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit) &&
             hit.collider == mc
         )
         {
-            if (mousePos != hit.point) mousePos = hit.point;
+            if (mousePos != hit.textureCoord) mousePos = hit.textureCoord;
         }
         else
         {
@@ -114,8 +151,8 @@ public class ComputeVertexLitPlane : MonoBehaviour
 
         //Run compute shader
         shader.SetVector("_MousePos", mousePos);
-        shader.SetFloat("_Time",Time.time);
         shader.Dispatch (_kernelHeightMap, dispatchCountHeightMap.x , dispatchCountHeightMap.y, 1);
+        shader.Dispatch (_kernelNormalMap, dispatchCountNormalMap.x , dispatchCountNormalMap.y, 1);
         shader.Dispatch (_kernel, dispatchCount , 1, 1);
     }
 
