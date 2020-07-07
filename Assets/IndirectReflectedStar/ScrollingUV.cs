f7e58ca6c43bab3502d042d9e0e585bb94f1f75c
MingWai <cmwdes@gmail.com>
2020-07-08 00:14:38 +0200

    initial assets

---
+++ Assets/IndirectReflectedStar/ScrollingUV.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ScrollingUV : MonoBehaviour
+{
+    public Vector2 speed;
+    public Material material;
+
+    private Vector2 uvOffset = Vector2.zero;
+
+    void Update()
+    {
+        uvOffset += Time.deltaTime*speed;
+        material.SetTextureOffset("_MainTex",uvOffset);
+    }
+}
