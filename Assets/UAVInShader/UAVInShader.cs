8fed835860c3635168634e0f189ae2616450b36f
MingWai Chan <mingwai@unity3d.com>
2021-10-26 15:17:25 +0200

    cleanup UAV after use to prevent crash

--- Assets/UAVInShader/UAVInShader.cs
+++ Assets/UAVInShader/UAVInShader.cs
@@ -17,6 +17,7 @@ public class UAVInShader : MonoBehaviour
 
     void OnDisable()
     {
+        Graphics.ClearRandomWriteTargets(); //this prevent crash
         if (fieldbuf != null)
         {
             fieldbuf.Release();
