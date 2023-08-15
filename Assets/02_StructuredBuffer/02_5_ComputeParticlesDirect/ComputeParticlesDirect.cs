6c3634bdb648b66e4a0f401db7665d0d11335788
MingWai Chan <mingwai@unity3d.com>
2023-08-15 18:04:07 +0200

    ported ComputeParticlesDirect to RenderGraph

--- Assets/02_StructuredBuffer/02_5_ComputeParticlesDirect/ComputeParticlesDirect.cs
+++ Assets/02_StructuredBuffer/02_5_ComputeParticlesDirect/ComputeParticlesDirect.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.Experimental.Rendering.RenderGraphModule;
 
 [CreateAssetMenu]
 public class ComputeParticlesDirect : ScriptableRendererFeature
@@ -15,9 +16,10 @@ public class ComputeParticlesDirect : ScriptableRendererFeature
 	public ComputeShader computeShader;
 	public RenderPassEvent evt;
 
-	private ComputeBuffer buffer;
+	private GraphicsBuffer buffer;
 	private Particle[] plists;
 	private bool reinit = false;
+	public string bufferName = "particleBuffer";
     
 	public ComputeParticlesDirect()
 	{
@@ -35,12 +37,12 @@ public class ComputeParticlesDirect : ScriptableRendererFeature
 		
 		//Set data to buffer
 		CleanUp();
-		buffer = new ComputeBuffer(count, 12); // 12 = sizeof(Particle)
+		buffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, count, 12); // 12 = sizeof(Particle)
 		buffer.SetData(plists);
 		
 		//Set buffer to computeShader and Material
-		computeShader.SetBuffer(0, "particleBuffer", buffer);
-		mat.SetBuffer ("particleBuffer", buffer);
+		computeShader.SetBuffer(0, bufferName, buffer);
+		mat.SetBuffer (bufferName, buffer);
 	}
 
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
@@ -77,15 +79,20 @@ public class ComputeParticlesDirect : ScriptableRendererFeature
 		private int count;
 		private Material mat;
 		private ComputeShader computeShader;
-		private ComputeBuffer buffer;
+		private GraphicsBuffer buffer;
+		private string passName;
+		private ProfilingSampler sampler;
 
-        public ComputeParticlesDirectPass(RenderPassEvent renderPassEvent, int count,Material material,ComputeShader computeShader,ComputeBuffer buffer)
+        public ComputeParticlesDirectPass(RenderPassEvent renderPassEvent, int count,Material material,ComputeShader computeShader,GraphicsBuffer buffer)
         {
             this.count = count;
             this.mat = material;
             this.renderPassEvent = renderPassEvent;
             this.computeShader = computeShader;
             this.buffer = buffer;
+
+			passName = GetType().Name;
+			sampler = new ProfilingSampler(passName);
         }
 
         public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescripor)
@@ -95,7 +102,7 @@ public class ComputeParticlesDirect : ScriptableRendererFeature
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
 			var colorHandle = renderingData.cameraData.renderer.cameraColorTargetHandle;
-			CommandBuffer cmd = CommandBufferPool.Get("ComputeParticlesDirectPass");
+			CommandBuffer cmd = CommandBufferPool.Get(passName);
             cmd.SetRenderTarget(colorHandle);
 			cmd.DispatchCompute(computeShader,0,Mathf.CeilToInt(count / 32),1,1);
 			cmd.DrawProcedural(Matrix4x4.identity,mat,0,MeshTopology.Points,1,count);
@@ -103,5 +110,46 @@ public class ComputeParticlesDirect : ScriptableRendererFeature
             context.ExecuteCommandBuffer(cmd);
 			CommandBufferPool.Release(cmd);
 		}
+
+        internal class PassData_DispatchCompute
+        {
+        }
+
+        internal class PassData_RenderParticles
+        {
+			public TextureHandle colorHandle;
+        }
+
+       public override void RecordRenderGraph(RenderGraph renderGraph, FrameResources frameResources, ref RenderingData renderingData)
+       {
+			//Dispatch compute
+			using (var builder = renderGraph.AddComputePass<PassData_DispatchCompute>(passName+"_DispatchCompute", out var passData, sampler))
+			{
+				//The compute will be culled because attachment dimensions is 0x0x0, so here we make sure it is not culled
+				builder.AllowPassCulling(false);
+
+				//Render function
+				builder.SetRenderFunc((PassData_DispatchCompute data, ComputeGraphContext rgContext) =>
+				{
+					rgContext.cmd.DispatchCompute(computeShader,0,Mathf.CeilToInt(count / 32),1,1);
+				});
+			}
+
+			//Render particles
+			var renderer = renderingData.cameraData.renderer as UniversalRenderer;
+			TextureHandle colorHandle = renderer.activeColorTexture;
+
+			using (var builder = renderGraph.AddRasterRenderPass<PassData_RenderParticles>(passName+"_RenderParticles", out var passData, sampler))
+			{   
+				//Setup passData
+				passData.colorHandle = builder.UseTextureFragment(colorHandle, 0, IBaseRenderGraphBuilder.AccessFlags.Write);
+
+				//Render function
+				builder.SetRenderFunc((PassData_RenderParticles data, RasterGraphContext rgContext) =>
+				{
+					rgContext.cmd.DrawProcedural(Matrix4x4.identity,mat,0,MeshTopology.Points,1,count);
+				});
+			}
+	   }
     }
 }
